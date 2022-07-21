3fdee05e89e2225d1b93b6f40fbee99e675b27f5
inkfromblood <104726891+inkfromblood@users.noreply.github.com>
2022-07-21 07:01:58 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 
-
 const app = express();
 
 var items = ["Buy Food", "Cook Food", "Eat Food"];
@@ -9,6 +8,7 @@ var items = ["Buy Food", "Cook Food", "Eat Food"];
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
 
 app.get("/", function(req, res) {
 
